a5796598616757d88e81b34a4b6e446c5b5fb95e
Armarcax <arcaxa305@gmail.com>
2026-08-09 02:25:48 +0400

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc8320127020a037"; }
+/******/ 		__webpack_require__.h = function() { return "5c5fd14cacf0d656"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
